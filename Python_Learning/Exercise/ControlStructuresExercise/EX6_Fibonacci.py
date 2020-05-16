591e5af010c4ab8e560de2f71cd9b4551fb54a7b
raghuveer <raghuveer@192.168.0.100>
2020-05-16 10:02:04 +0530

    Exercise Set 1

--- Python_Learning/Exercise/ControlStructuresExercise/EX6_Fibonacci.py
+++ Python_Learning/Exercise/ControlStructuresExercise/EX6_Fibonacci.py
@@ -5,3 +5,29 @@ Write a Python program to print first n Fibonacci numbers.
 
 @author: raghuveer
 '''
+
+def fibonacci(num):
+    f0 = 0
+    f1 = 1
+    temp = 0
+    fib_list = []
+    if num == 1:
+        fib_list.append(f0)
+    elif num == 2:
+        fib_list.append(f0)
+        fib_list.append(f1)
+    else:
+        fib_list.append(f0)
+        fib_list.append(f1)
+        while (num > 2):
+            fib_list.append(f0+f1)
+            num -= 1
+            temp = f0 + f1
+            f0 = f1
+            f1 = temp
+    return fib_list
+
+num = int(input("Input a number:"))
+
+print(fibonacci(num))
+
